f5e37e6d3f49c220eca06db37daf8d820933a037
Taikumi <Taikumi@gmail.com>
2012-03-16 15:41:11 -0400

    Added test script to run tests.

---
+++ test.py
@@ -0,0 +1,5 @@
+from crawly import Crawly
+
+
+urls = ['http://appworld.blackberry.com/webstore/content/42040/?lang=en', 'http://appworld.blackberry.com/webstore/content/42040/?lang=en']
+Crawly(urls).start()
